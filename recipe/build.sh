2b333592c20befa1d548cf09bd2c3299c4aee219
Simon Bray <32272674+simonbray@users.noreply.github.com>
2019-11-29 00:03:07 +0100

    Update build.sh

--- recipe/build.sh
+++ recipe/build.sh
@@ -1,5 +1,9 @@
 export C_INCLUDE_PATH="$PREFIX/include:$C_INCLUDE_PATH"
 
+if [ `uname` == Darwin ]; then
+  export CC="${CC} -fopenmp"
+fi
+
 mkdir build && cd build
 cmake ..
 echo "CXX_INCLUDES = -isystem $PREFIX/include" >> $SRC_DIR/build/src/CMakeFiles/fastpca.dir/flags.make
